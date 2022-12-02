95051d46c3bcb96b0c54c3b46419443b0d579aeb
aranajad <111802980+aranajad@users.noreply.github.com>
2022-12-02 14:49:04 -0500

    Update
    64BitRegister.vhd -> Register_64.vhd
    
    Basic VHDL outline added to both Register_64 and registerToLED

--- Register_64.vhd
+++ Register_64.vhd
@@ -1,18 +1,17 @@
 --Library Declaration
 library IEEE;
-use IEEE.std_logic_1164.all;
+use IEEE.std_logic_1164.all
 
 --Entity Declaration
-entity registerToLed is
+entity Register_64 is
     port (
         clk : in std_logic;
         rst : in std_logic;
         sig
     );
-    
-end registerToLed;
+end Register_64;
 
-architecture rtl of registerToLed is
+architecture rtl of Register_64 is
 
 
 
